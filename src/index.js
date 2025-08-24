2c94e6d1faaa5cd96e9cbb797a730bdf41806124
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-24 15:47:05 +0300

    Finally

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 function calcFactorial(n) {
-    // if (n < 0) return null;
-    if (n === 1 || n === 0) return 1;
+    if (n <= 1) return 1;
     return n * calcFactorial(n - 1);
 }
 
-const targetNum = 5;
+const targetNum = 10;
 
 targetNum < 0 || !Number.isInteger(targetNum)
-    ? console.log(`Factorial can't be calculated for ${targetNum}`)
+    ? console.log(`Factorial is not defined for ${targetNum}`)
     : console.log(`Factorial of ${targetNum} is ${calcFactorial(targetNum)}`);
